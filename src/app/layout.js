15c35505dd17d8b61afbafc40df15c748855fae3
grahamusai <grahamusai0@gmail.com>
2024-02-06 14:12:59 +0200

    innitial commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,11 @@
-import { Inter } from "next/font/google";
+import { Krona_One } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+
+const krona_One = Krona_One({ 
+  weight: ["400"],
+  subsets: ["latin"] });
+
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={krona_One.className}>{children}</body>
     </html>
   );
 }
