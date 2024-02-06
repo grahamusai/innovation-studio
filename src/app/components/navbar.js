c2893029778d64d6a7c3f744fcdcf4542784052f
grahamusai <grahamusai0@gmail.com>
2024-02-06 16:27:37 +0200

    Setup gsap

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Navbar() {
   return (
     <div>
-        <nav className="bg-white text-xs bg-opacity-0  w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+        <nav className="bg-white text-xs bg-opacity-0 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
           <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
               <span className="self-center  whitespace-nowrap dark:text-white">Innovatio Studio</span>
--- src/app/page.js
+++ src/app/page.js
@@ -1,39 +1,50 @@
 import Image from "next/image";
 import Navbar from "./components/navbar";
+import SplitType from "split-type";
+import { gsap } from "gsap";
+import { TextPlugin } from "gsap/TextPlugin";
 
 export default function Home() {
-  
-  return (
-    <main className=" py-24 px-24">
-      <Navbar />
-      <h1 className="text-[4rem] font-black text-white ">REACHING THE <br /><span className="text-black font-thin font-outline-2">VERTEX OF  </span>DESIGN<br />EXCELLENCE</h1>
-      <div class="container mx-auto py-8">
-    <div class="flex flex-wrap -mx-4">
 
-        <div class="w-full md:w-1/3 px-4 mb-8">
-            <div class="bg-white p-6 rounded-lg shadow-md">
-                <h2 class="text-xl text-black text-right font-semibold uppercase pb-3 text-[3rem] mb-4">Port</h2>
-                <h2 class="text-xl text-black text-right font-semibold uppercase pb-3 text-[3rem] mb-4">foli</h2>
-                <h2 class="text-xl text-black text-right font-semibold uppercase text-[3rem] mb-4">o</h2>
+
+    return (
+        <main className=" py-24 px-24">
+            <Navbar />
+            <h1 className="text-[4rem] font-black text-white" id="my-text">REACHING THE <br /><span className="text-black font-thin font-outline-2 ml-12">VERTEX OF  </span>DESIGN<br />EXCELLENCE</h1>
+            <div className="container mx-auto">
+                <div className="flex flex-wrap -mx-4">
+
+                    <div className="w-full md:w-1/3 px-4 ">
+                        <div className="bg-white rounded-lg shadow-md">
+                            <p className=" text-black text-right font-semibold uppercase text-[4rem] ">Port<br />foli <br />o</p>
+                        </div>
+                    </div>
+
+                    <div className="w-full md:w-1/3">
+                        <div className="bg-white rounded-lg p-4 shadow-md">
+                            <div className="flex">
+                                <div className="w-full md:w-1/2 mx-2">
+                                    <p className=" bg-orange-500 py-2 text-center rounded-md">Lets Talk</p>
+                                </div>
+                                <div className="w-full md:w-1/2">
+                                    <p className="text-black">About Our<br /> Amazing</p>
+                                </div>
+                            </div>
+
+                            <h2 className="text-black font-semibold text-center text-[4rem]">Services</h2>
+                            <p>Content goes here.</p>
+                        </div>
+                    </div>
+
+                    <div className="w-full md:w-1/3 px-4 ">
+                        <div className="bg-white p-6 rounded-lg shadow-md">
+                            <h2 className="text-xl font-semibold">Column 3</h2>
+                            <p>Content goes here.</p>
+                        </div>
+                    </div>
+
+                </div>
             </div>
-        </div>
-
-        <div class="w-full md:w-1/3 px-4 mb-8">
-            <div class="bg-white py-4 px-2 rounded-lg shadow-md">
-                <h2 class="text-xl text-black font-semibold text-center text-[3rem] mb-4">Services</h2>
-                <p>Content goes here.</p>
-            </div>
-        </div>
-
-        <div class="w-full md:w-1/3 px-4 mb-8">
-            <div class="bg-white p-6 rounded-lg shadow-md">
-                <h2 class="text-xl font-semibold mb-4">Column 3</h2>
-                <p>Content goes here.</p>
-            </div>
-        </div>
-
-    </div>
-</div>
-    </main>
-  );
+        </main>
+    );
 }
